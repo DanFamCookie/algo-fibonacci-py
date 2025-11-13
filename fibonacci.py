0eaebf91b5747a534141af08ed21b9604c8aa4b7
YOUR_NAME <YOUR_EMAIL>
2025-11-12 18:57:19 -0800

    Code finished I looked at Javascript Fibonacci and tried to translate it to Python

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,13 @@
 def fibonacci(n):
+  # this code is the base case which means if n is 0 or 1 it prints it out because of the fibonacci sequence 
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+
+  return fibonacci(n - 1) + fibonacci(n - 2)
+#this is the fibonacci sequence 
   pass
 
 
+print(fibonacci(4))
